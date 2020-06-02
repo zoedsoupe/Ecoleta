dbc2058afa17df9cd5a355b30ebec68443e90711
Zoey <zoey.spessanha@zeetech.io>
2020-06-02 10:28:09 -0300

    http methods and params

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,41 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  console.log("Listagem de usuários");
+app.use(express.json());
 
-  res.json(["Diego", "Robson", "Cleyton"]);
+//* Rota: endereço completo da requisição
+//* Recurso: qual a entendidade estamos acessando do sistema
+
+//* Request Params: parâmetros que vem na própria rota, obrigatŕio
+//* Query Params: parâmetros que vem na rota, geralmente opcionais, para paginação e filtros
+//* Request Body: parâmetros para criação/atualização de informações
+
+const users = ["Diego", "Robson", "Cleyton", "Daniel"];
+
+app.get("/users", (req, res) => {
+  const search = String(req.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  return res.json(filteredUsers);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = users[id];
+
+  return res.json(user);
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+
+  const user = data;
+
+  return res.json(user);
 });
 
 app.listen(3333, () => console.log("Ecoleta started!"));
