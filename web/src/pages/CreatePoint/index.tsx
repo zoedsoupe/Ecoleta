a38cac9b379c1a448f9f18453765eb8aa2d8870e
Zoey <zoey.spessanha@zeetech.io>
2020-06-03 10:48:41 -0300

    routes

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./CreatePoint.css";
+
+const CreatePoint = () => {
+  return <h1>Hello Planet</h1>;
+};
+
+export default CreatePoint;
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {FiLogIn} from "react-icons/fi"
+import { FiLogIn } from "react-icons/fi";
+
 
 import "./Home.css";
 import logo from "../../assets/logo.svg";
@@ -12,10 +13,12 @@ const Home = () => {
         </header>
         <main>
           <h1>Seu marketplace de coleta de resíduos</h1>
-          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente!</p>
-          <a href="/cadastro">
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente!
+          </p>
+          <a href="/create-point">
             <span>
-              <FiLogIn/>
+              <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
           </a>
