8e9b714a6ec652dbeeb05b3d457818bc6b0a2c6e
Zoey <zoey.spessanha@zeetech.io>
2020-06-03 12:33:05 -0300

    handle multiple items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -43,6 +43,7 @@ const CreatePoint = () => {
 
   const [selectedUf, setSelectedUf] = useState("0");
   const [selectedCity, setSelectedCity] = useState("0");
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([
     0,
     0,
@@ -107,6 +108,10 @@ const CreatePoint = () => {
     const { name, value } = e.target;
 
     setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSelectedItems(id: number) {
+    setSelectedItems([...selectedItems, id]);
   }
 
   return (
@@ -220,7 +225,11 @@ const CreatePoint = () => {
 
           <ul className="items-grid">
             {items.map((item) => (
-              <li key={item.id}>
+              <li
+                key={item.id}
+                onClick={() => handleSelectedItems(item.id)}
+                className={selectedItems.includes(item.id) ? "selected" : ""}
+              >
                 <img src={item.image_url} alt={item.title} />
                 <span>{item.title}</span>
               </li>
