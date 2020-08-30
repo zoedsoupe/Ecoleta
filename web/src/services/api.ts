bc340f5db8053b5d2992dac595a472167b7be695
Zoey <zoey.spessanha@zeetech.io>
2020-08-30 13:06:59 -0300

    corrected api

--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://ecoleta-4nzhshqzha-uc.a.run.app/",
 });
 
-export default api;
+export { api };
